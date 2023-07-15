27507884ab63e891d37b8b5b5aa74469934c8896
Yash Solanki <ysolanki2013@gmail.com>
2023-07-15 16:31:22 +0530

    List products on home screen

---
+++ frontend/src/screens/HomeScreen.jsx
@@ -0,0 +1,20 @@
+import { Row, Col } from "react-bootstrap";
+import products from "../products";
+import Products from "../components/Products";
+
+const HomeScreen = () => {
+  return (
+    <>
+      <h1>Latest Products</h1>
+      <Row>
+        {products.map((product) => (
+          <Col sm={12} md={6} lg={4} xl={3}>
+            <Products product={product} />
+          </Col>
+        ))}
+      </Row>
+    </>
+  );
+};
+
+export default HomeScreen;
