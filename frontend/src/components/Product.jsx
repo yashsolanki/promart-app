9e1ee09bb2bc369cc671bbbab7cb2f1750c5f2f2
Yash Solanki <ysolanki2013@gmail.com>
2023-07-15 23:40:24 +0530

    Rating component

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -1,7 +1,8 @@
 import { Link } from "react-router-dom";
 import { Card } from "react-bootstrap";
+import Rating from "./Rating";
 
-const Products = ({ product }) => {
+const Product = ({ product }) => {
   return (
     <Card className="my-3 p-3 rounded">
       <Link to={`/product/${product._id}`}>
@@ -9,14 +10,20 @@ const Products = ({ product }) => {
       </Link>
       <Card.Body>
         <Link to={`/product/${product._id}`}>
-          <Card.Title as="div">
+          <Card.Title as="div" className="product-title">
             <strong>{product.name}</strong>
           </Card.Title>
         </Link>
+        <Card.Text as="div">
+          <Rating
+            value={product.rating}
+            text={`${product.numReviews} reviews`}
+          />
+        </Card.Text>
         <Card.Text as="h3">₹{product.price}</Card.Text>
       </Card.Body>
     </Card>
   );
 };
 
-export default Products;
+export default Product;
--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,6 +1,6 @@
 import { Row, Col } from "react-bootstrap";
 import products from "../products";
-import Products from "../components/Products";
+import Product from "../components/Product";
 
 const HomeScreen = () => {
   return (
@@ -9,7 +9,7 @@ const HomeScreen = () => {
       <Row>
         {products.map((product) => (
           <Col sm={12} md={6} lg={4} xl={3}>
-            <Products product={product} />
+            <Product product={product} />
           </Col>
         ))}
       </Row>
