426f061d26d21b9bbd653299720a5af38389679f
Yash Solanki <ysolanki2013@gmail.com>
2023-07-16 16:42:52 +0530

    Environment variables setup

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,10 @@
 import express from "express";
+import dotenv from "dotenv";
 import products from "./data/products.js";
 
-const port = 5000;
+dotenv.config();
+
+const port = process.env.PORT || 5000;
 
 const app = express();
 
