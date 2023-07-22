990a017bc85f608b02788d7ddf1d019ce98dd683
Yash Solanki <ysolanki2013@gmail.com>
2023-07-22 18:29:18 +0530

    Login functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
 import { Outlet } from "react-router-dom";
-import Header from "./components/Header";
 import { Container } from "react-bootstrap";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Header from "./components/Header";
 import Footer from "./components/Footer";
 
 const App = () => {
@@ -13,6 +15,7 @@ const App = () => {
         </Container>
       </main>
       <Footer />
+      <ToastContainer />
     </>
   );
 };
