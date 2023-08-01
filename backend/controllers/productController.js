bf6f5d0c73a4f1335f093f4c1cf201d6952ea827
Yash Solanki <ysolanki2013@gmail.com>
2023-08-01 17:14:56 +0530

    Thorwing error from productControllers gives a custom error response

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -79,10 +79,10 @@ const deleteProduct = asyncHandler(async (req, res) => {
   const product = await Product.findById(req.params.id);
   if (product) {
     await Product.deleteOne({ _id: product._id });
-    res.status(200).json({ message: "Product deleted" });
+    res.status(200).json({ message: "Product removed" });
   } else {
     res.status(404);
-    throw new Error("Resource not found");
+    throw new Error("Product not found");
   }
 });
 
